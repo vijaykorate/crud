090842ecc787191775e91a5a0149b805b229137a
vijaykorate <vijaykorate18@gmail.com>
2025-05-25 00:25:16 +0530

    update array.js

--- src/components/array.js
+++ src/components/array.js
@@ -12,6 +12,46 @@ const array = [
         id: "2",
         Name: "Simran",
         Age: "22",
+    },
+    {
+        id: "3",    
+        Name: "Sahil",
+        Age: "24",
+    },
+    {
+        id: "4",
+        Name: "Anjali",
+        Age: "21",
+    },
+    {
+        id: "5",
+        Name: "Rahul",
+        Age: "25",
+    },
+    {
+        id: "6",
+        Name: "Priya",
+        Age: "20",
+    },
+    {
+        id: "7",
+        Name: "Karan",
+        Age: "26",
+    },
+    {
+        id: "8",
+        Name: "Neha",
+        Age: "19",
+    },
+    {
+        id: "9",
+        Name: "Rohan",
+        Age: "27",
+    },
+    {
+        id: "10",
+        Name: "Pooja",
+        Age: "28",
     }
 ];
 
